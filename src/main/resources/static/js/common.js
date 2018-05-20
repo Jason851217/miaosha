b09c32475e2aebc45769184b4c71f6ffbe8506e5
Jason <cindy851217@gmail.com>
2018-05-21 00:10:13 +0800

    miaosha

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -8,7 +8,7 @@ var g_passsword_salt="1a2b3c4d"
 // 获取url参数
 function g_getQueryString(name) {
 	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-	var r = window.location.git config --get core.autocrlfsearch.substr(1).match(reg);
+	var r = window.location.search.substr(1).match(reg);
 	if(r != null) return unescape(r[2]);
 	return null;
 };
